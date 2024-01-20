2e759015cce691bf2dedc9af2e9d83c5ade9b3c4
Bishal Bhattacharjee <bishalbhattacharjee55@gmail.com>
2024-01-20 10:47:06 -0800

    ok

---
+++ 05bgChanger/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("black");
+
+  return (
+   <div className="w-full h-screen " style={{backgroundColor:color}}>
+    <div className="flex flex-wrap justify-center bottom-12 inset-x-0 px-2" >
+       <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl mt-10  ">
+        <button onClick={() => {setColor("red")}}className="outline-none px-4 text-white rounded-full " style={{backgroundColor:"red"}}>
+ Red
+        </button>
+        <button onClick={() => {setColor("pink")}} className="outline-none px-4 text-white rounded-full " style={{backgroundColor:"pink"}}>
+ Pink
+        </button>
+        <button onClick={() => {setColor("blue")}}  className="outline-none px-4 text-white rounded-full " style={{backgroundColor:"blue"}}>
+Blue
+        </button>
+        <button onClick={() => {setColor("yellow")}}  className="outline-none px-4 text-white rounded-full " style={{backgroundColor:"yellow"}}>
+Yellow
+        </button>
+        <button  onClick={() => {setColor("green")}}  className="outline-none px-4 text-white rounded-full " style={{backgroundColor:"green"}}>
+Green
+        </button>
+        <button  onClick={() => {setColor("violet")}}  className="outline-none px-4 text-white rounded-full " style={{backgroundColor:"violet"}}>
+ Violet
+        </button>
+
+       </div>
+    </div>
+   </div>
+  );
+}
+
+export default App;
--- viteproject4/src/App.jsx
+++ viteproject4/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //const [count, setCount] = useState(0)
+  let myobj = {
+    name:"Bishal",
+    age:22
+  }
+  let myarr=[1,2,3]
   return (
     <>
-     <h1 className="text-2xl bg-slate-600">Tailwind with React</h1>
+     <h1 className="text-2xl bg-violet-400 rounded-xl mb-6">Tailwind with React</h1>
+    <Card username="Bishal" others={myarr} btnclick="click me" />
+    
+
+   
     </>
   )
 }
