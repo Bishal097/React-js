41f01ae1dc0544bddec1d6a0f663b79caed1e8e0
Bishal Bhattacharjee <bishalbhattacharjee55@gmail.com>
2024-01-24 09:18:56 -0800

    chai

--- 07CurrencyGenerator/src/hooks/useCurrencyInfo.js
+++ 07CurrencyGenerator/src/hooks/useCurrencyInfo.js
@@ -4,7 +4,7 @@ function useCurrencyInfo(currency)
 {
     const [data,setData]=useState({})
     useEffect(()=>{
-      fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/{currency}.json')
+      fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
       .then((res) => res.json())
       .then((res) => setData(res[currency]))
     },[currency])
