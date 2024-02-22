5d05851b9f0e873111defab8cc7a6ac0a0c57c68
Bishal Bhattacharjee <bishalbhattacharjee55@gmail.com>
2024-02-22 10:23:13 +0530

    redux

---
+++ reduxToolkittodo/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Chalo hm chale ab hm chale</h1>
+    <AddTodo/>
+    <Todos/>
+    
+    </>
+  )
+}
+
+export default App
